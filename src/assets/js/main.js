73674700c4647c0b7a5e9bc0ac1ccd13d33db4f7
vrenaldi <vrenaldi258@gmail.com>
2017-09-22 09:47:43 +0200

    Initial configuration.

---
+++ src/assets/js/main.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+import '../css/main.css';
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 'hotpink' });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+var animate = function () {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+
+    renderer.render(scene, camera);
+};
+
+animate();
+
+console.log('Hello world!!');
+
+// https://github.com/webpack/webpack-dev-server/issues/1101
---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/assets/js/main.js',
+    output: {
+        filename: 'assets/js/bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['build']),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        })
+    ]
+};
